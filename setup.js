c63621ec19d3f23e95a00f95a69b19487ef23de5
toBeOfUse <mitchjacov@gmail.com>
2022-11-29 10:02:08 -0500

    code for db setup (not for lesson)

---
+++ setup.js
@@ -0,0 +1,17 @@
+db.createRole({
+  role: "hacksu",
+  privileges: [
+    { resource: { db: "pokemon", collection: "pokedex" }, actions: ["find"] },
+    {
+      resource: { db: "pokemon", collection: "PC" },
+      actions: ["find", "update", "insert"],
+    },
+  ],
+  roles: [],
+});
+
+db.createUser({
+  user: "student",
+  pwd: "[[[[[PASSWORD]]]]]",
+  roles: ["hacksu"],
+});
