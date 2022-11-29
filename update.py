ea4bb7f879b0f90c8284c5be2c989b11679a1879
toBeOfUse <mitchjacov@gmail.com>
2022-11-29 15:04:08 -0500

    add update code

---
+++ update.py
@@ -0,0 +1,28 @@
+from pprint import pprint
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+client = MongoClient(
+    host="mitch.website",
+    username="student",
+    password="[[password]]"
+)
+db = client.pokemon
+collection = db.PC
+
+my_id = ObjectId("your id")
+id_query = { "_id": my_id }
+
+print("updating owner...")
+collection.update_one(
+    id_query,
+    {"$set": {"owner": "Mitch"}}
+)
+pprint(collection.find_one(id_query))
+
+print("incrementing health...")
+collection.update_one(
+  id_query,
+  { "$inc": {"current_hp": 5} }
+)
+pprint(collection.find_one(id_query))
